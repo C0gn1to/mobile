deb42bd89c0ebc809101deec8dec865e7116a9a8
C0gn1to <2emejetable@gmail.com>
2024-08-23 11:44:42 +0200

    dev script

--- dev.sh
+++ dev.sh
@@ -3,4 +3,6 @@
 git submodule init 
 git submodule update --remote --merge
 docker compose up -d
-docker exec -it flutter flutter run
+docker wait flutter  # Waiting for it to finish building
+docker start flutter
+docker exec -it flutter /bin/bash -c "adb connect emulator:5555 && flutter run"
